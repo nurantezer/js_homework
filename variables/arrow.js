315fa93b99b66eb066d557c2dde53fe8df78ec4c
nurantezer <nurantezer68@gmail.com>
2022-12-28 14:34:20 +0300

    calculate

--- variables/arrow.js
+++ variables/arrow.js
@@ -1,16 +1,21 @@
 // Silindirin hacmi
 ///hacim= pi*r*r*h
 
-const r = +prompt("r:")
-const h = +prompt("h:")
+// const r = +prompt("r:")
+// const h = +prompt("h:")
 
-const volume = (r,h) => Math.PI *r *r *h
-console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);
+// const volume = (r,h) => Math.PI *r *r *h
+// console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);
 
-//!Alternatif olarak
-const volumeLong = (r,h) => {
-    const result = Math.PI * r * r * h
-    console.log(result);
-    return result
-}
-console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);
+// //!Alternatif olarak
+// const volumeLong = (r,h) => {
+//     const result = Math.PI * r * r * h
+//     console.log(result);
+//     return result
+// }
+// console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);
+
+//Yas hesaplama
+
+const calcutateAge = (year) => new Date().getFullYear() - year
+console.log("Age:" , calcutateAge(1990));

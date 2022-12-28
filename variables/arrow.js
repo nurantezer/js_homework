7eafdea834b9e432a646a7552bbe00e763c40699
nurantezer <nurantezer68@gmail.com>
2022-12-28 14:28:38 +0300

    update

--- variables/arrow.js
+++ variables/arrow.js
@@ -6,3 +6,11 @@ const h = +prompt("h:")
 
 const volume = (r,h) => Math.PI *r *r *h
 console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);
+
+//!Alternatif olarak
+const volumeLong = (r,h) => {
+    const result = Math.PI * r * r * h
+    console.log(result);
+    return result
+}
+console.log(`${r},${h} = Volume: ${volume(r,h).toFixed(4)}`);

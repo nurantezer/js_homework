55898a4836b8f1451c2b78da21c8399ecd0b5d8e
nurantezer <nurantezer68@gmail.com>
2022-12-31 09:18:30 +0300

    kredi

---
+++ kredi.js
@@ -0,0 +1,5 @@
+const income = Number(prompt("your income"))
+const outcome = Number(prompt("your outcome"))
+const credibility =
+income - outcome >= 5500 ? "loan avalaıble🤑" : "loan not avalıble😔"
+console.log(credibility);

2e13f34aa9345ae53a6cfa3f9dbd4d47e70c2321
nurantezer <nurantezer68@gmail.com>
2023-01-05 16:22:47 +0300

    soru 2

--- variables/loops-sorular.js
+++ variables/loops-sorular.js
@@ -1,7 +1,19 @@
 //*Soru-1) 1000 ile 2000 arasındaki 13 e bölümünden 3 kalanı veren sayıları
 //*konsola yazdırınız
-for (let i = 1000; i < 2000; i++) {
-  if (i % 13 === 3) {
-    console.log(i);
-  }
+
+// for (let i = 1000; i < 2000; i++) {
+//   if (i % 13 === 3) {
+//     console.log(i);
+//   }
+// }
+
+//*Soru-2) Kullanıcıdan alınan 5 sayının ortalamasını konsola yazdırınız.
+
+let sayac = 0;
+let sum = 0;
+while (sayac < 5) {
+  const sayi = +prompt("bir sayi giriniz");
+  sum += sayi;
+  sayac++;
 }
+console.log(sum / 5);

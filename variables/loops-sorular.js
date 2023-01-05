842a23e07eabe05cb7a236f9de8b27925631c429
nurantezer <nurantezer68@gmail.com>
2023-01-05 16:13:17 +0300

    soru 1

---
+++ variables/loops-sorular.js
@@ -0,0 +1,7 @@
+//*Soru-1) 1000 ile 2000 arasındaki 13 e bölümünden 3 kalanı veren sayıları
+//*konsola yazdırınız
+for (let i = 1000; i < 2000; i++) {
+  if (i % 13 === 3) {
+    console.log(i);
+  }
+}

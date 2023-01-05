aab7060efb17ba68d71a90f587eecdc9d4a0a68d
nurantezer <nurantezer68@gmail.com>
2023-01-05 17:17:21 +0300

    soru 4

--- variables/loops-sorular.js
+++ variables/loops-sorular.js
@@ -21,15 +21,31 @@
 //*Soru-3 Kullanıcıdan alınan sayıları negatif girilene kadar
 //*toplayan ve konsola yazdıran kodu yazınız.
 
-let sum = 0;
-let flag = true;
+// let sum = 0;
+// let flag = true;
 
-while (flag) {
-  const sayi = +prompt("bir sayi giriniz");
-  if (sayi < 0) {
-    flag = false;
-  } else {
-    sum += sayi;
-  }
-}
-console.log(sum);
+// while (flag) {
+//   const sayi = +prompt("bir sayi giriniz");
+//   if (sayi < 0) {
+//     flag = false;
+//   } else {
+//     sum += sayi;
+//   }
+// }
+// console.log(sum);
+
+//* Soru-4 Sizden 1'den 100'e kadar sayıları yazdırmanız istenmektedir
+//*Ancak 3'ün katlarına gelince "fizz", 5'in katlarına gelince "buzz"
+//*Hem 3'ün hemde 5'in katlarına gelince "fizzbuzz" yazdırmanız istenmektedir.
+
+// for (let i = 1; i < 100; i++) {
+//   if (i % 15 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }

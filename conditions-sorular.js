b55c3f6addb6063a14df46e8de34899592629fcb
nurantezer <nurantezer68@gmail.com>
2023-01-08 11:04:16 +0300

    soru 2

--- conditions-sorular.js
+++ conditions-sorular.js
@@ -1,8 +1,14 @@
-//*Soru-1 Bir sayının tek mi çift mi olduğunu consola yazdıran koşulu yazınız
+// //*Soru-1 Bir sayının tek mi çift mi olduğunu consola yazdıran koşulu yazınız
 
-const sayi = 30;
-if (sayi % 2 === 0) {
-  console.log(`${sayi} bir çift sayıdır`);
-} else if (sayi % 2 === 1 || -1) {
-  console.log(`${sayi} bir tek sayıdır`);
-}
+// const sayi = 30;
+// if (sayi % 2 === 0) {
+//   console.log(`${sayi} bir çift sayıdır`);
+// } else if (sayi % 2 === 1 || -1) {
+//   console.log(`${sayi} bir tek sayıdır`);
+// }
+
+//*Soru-2 Bir sayının 0 ile 100 arasında olup olmadığını konsola yazdıran
+//*ternary yapııyı yazınız
+
+const sayi = -100;
+0 < sayi && sayi < 100 ? console.log(`sağlar`) : console.log(`sağlamaz`);

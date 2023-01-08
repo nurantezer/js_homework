0c933f62711c78e7fecfd530c932e530eb225e8f
nurantezer <nurantezer68@gmail.com>
2023-01-08 09:26:50 +0300

    soru 2

--- function-sorular.js
+++ function-sorular.js
@@ -6,3 +6,15 @@ function calFahrenheit(cel) {
   return cel + ` derece` + fahr + " fahrenheit'a eşittir";
 }
 console.log(calFahrenheit(5));
+
+//*Soru-2) Belirli bir string ifadenin tersini bulan fonksiyonu yazınız
+
+function reverseString(str) {
+  let str2 = ``;
+  for (let i = str.length - 1; i >= 0; i--) {
+    str2 += str[i];
+  }
+  return str2;
+}
+
+console.log(reverseString(`hello world`));

c839fdffa87e4fd8d472631e3fad784c29caee32
nurantezer <nurantezer68@gmail.com>
2023-01-08 09:44:18 +0300

    soru 3

--- function-sorular.js
+++ function-sorular.js
@@ -18,3 +18,25 @@ function reverseString(str) {
 }
 
 console.log(reverseString(`hello world`));
+
+//*Soru3 Belirli bir string ifadenin palindrome olup olmadığını bulan
+//*fonksiyonu yazınız.(Büyük, küçük harf duyarlılığı olmamalıdır)Ayna
+//*etkisi ile ortadan bölmesi "selles"
+
+const string = `seles`;
+
+function reverseString(str) {
+  let str2 = ``;
+  for (let i = str.length - 1; i >= 0; i--) {
+    str2 += str[i];
+  }
+  return str2;
+}
+
+const isPalindrome = function (string) {
+  const reversed = reverseString(string);
+  return reversed == string
+    ? `${string} bir palindrome`
+    : `${string} palindrome değil`;
+};
+console.log(isPalindrome(string));

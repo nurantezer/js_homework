7d29a7bc1ca7aa0a56894a46c4ca1097635b31d1
nurantezer <nurantezer68@gmail.com>
2023-01-15 08:27:25 +0300

    end

--- object-sorular.js
+++ object-sorular.js
@@ -156,7 +156,8 @@ const carData = [
 // console.log(newList1 / carData.length);
 
 //* 4- 8 silindirli araçları listele
-
+const eightCylinderCars = carData.filter((car) => car.engine.cylinders === 8);
+console.log(eightCylinderCars);
 //* 5 Farklı uzunlukta olması muhtemel iki listeden İlki key'lerden, ikincisi ise Value'lardan oluşmaktadır.
 //* Yine key ve value'lardan oluşan bir obje döndüren bir fonksiyon yazınız. Y
 //* eterli value yoksa, kalan keylerin değeri null olmalıdır. Yeterli anahtar yoksa,
@@ -174,5 +175,44 @@ const createDict = (keys, values) => {
 };
 //* 6- Bir obje içerisindeki key ve value'ları değiştirin. Yani key'ler value'lar olmalı ve
 //*value'lar key'ler olmalıdır.
-// Örnek: {a: 1, b: 2, c: 3}  -->  {1: 'a', 2: 'b', 3: 'c'}
-// Not: Eğer bir value birden fazla key'e sahipse, son key'i kullanın.
+//* Örnek: {a: 1, b: 2, c: 3}  -->  {1: 'a', 2: 'b', 3: 'c'}
+//* Not: Eğer bir value birden fazla key'e sahipse, son key'i kullanın.
+let obj = { a: 1, b: 2, c: 3 };
+function reverseDict(obj) {
+  let newObj = {};
+  for (let key in obj) {
+    newObj[obj[key]] = key;
+  }
+  return newObj;
+}
+console.log(reverseDict(obj));
+
+// 6-
+// Size bazı dilleri ve verilen dillerdeki test sonuçlarınızı içeren bir dictionarj obj verilir. Test puanınızın en az 60 olduğu dillerin listesini sonuçların azalan sırasına göre döndürün.
+// Not: puanlar her zaman benzersiz olacaktır (bu nedenle yinelenen değerler olmayacaktır)
+// örnekler
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+// çözüm
+let results = { Java: 10, Ruby: 80, Python: 95 };
+function myLanguages(results) {
+  let filteredObj = {};
+  let arr = [];
+  for (let key in results) {
+    if (results[key] >= 60) {
+      filteredObj[key] = results[key];
+    }
+  }
+  arr = Object.values(filteredObj).sort((a, b) => b - a);
+  let sortedObj = {};
+  arr.forEach((value) => {
+    for (let key in filteredObj) {
+      if (filteredObj[key] === value) {
+        sortedObj[key] = value;
+      }
+    }
+  });
+  return sortedObj;
+}
+console.log(myLanguages(results));

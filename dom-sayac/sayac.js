f1788864c760372eb90972ee96a4ecde661f5805
nurantezer <nurantezer68@gmail.com>
2023-01-19 13:28:08 +0300

    son

---
+++ dom-sayac/sayac.js
@@ -0,0 +1,32 @@
+//!set inital value to zero
+let count = 0;
+//!select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count = 0;
+        }
+
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if (count < 0){
+            value.style.color = "red";
+        }
+        if (count === 0){
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    })
+})
+
+//!const styles = e.currentTarget.classList; burada seçimim queryselectorall ile olduğu için nodelist üzerinde foreach
+//!ile devam ettim ve burada currentTarget ile classListteki nodeleri seçip contains metodu ile içine girdim.

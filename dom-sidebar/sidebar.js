11294ab0cd6d7e390cd51561cf9d7e1d6a4fc110
nurantezer <nurantezer68@gmail.com>
2023-01-21 11:18:31 +0300

    dom-sidebar

---
+++ dom-sidebar/sidebar.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//!Bu proje de css olarak transfor: transtlate (-100%) girildi çünkü bu özellik sidebarı ekrandan kal
+//!dırıyor .show-sidebar {transform: translate(0)} yapıldı çünkü bu da sidebar çağırılfığında
+//!getirecek

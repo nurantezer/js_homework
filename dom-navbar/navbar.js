b544759018b5c6a82f191c825290c0cec8422980
nurantezer <nurantezer68@gmail.com>
2023-01-21 09:51:56 +0300

    end

---
+++ dom-navbar/navbar.js
@@ -0,0 +1,23 @@
+//* classList - shows/gets all classes
+//* contains - checks classList for specific class
+//* add - add class
+//* remove - remove class
+//* toggle - toggles class
+//* Bu sayfada responsive var
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random")); böyle bir şey yok tabi
+    // console.log(links.classList.contains("links")); bu var.
+    //* if(links.classList.contains("show-links")){
+    //*     links.classList.remove("show-links");
+    //* }else{
+    // *    links.classList.add("show-links");
+    //* }
+    links.classList.toggle("show-links");  //tıklandığında göster.
+})
+
+//!BU projede css kısmındaki .links{} önemli çünkü heigthini auto vermezsem görünmez.
